70afb5f9a7cdd31b2c82bbbfa21742caf260b957
Eka <putu_eka_terima@yahoo.de>
2024-10-29 01:11:59 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -8,8 +8,13 @@
 </main>
 
 <style>
-  /* Import Google Font (e.g., 'Press Start 2P' for a retro style) */
+  /* Import Google Font */
   @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+
+  /* Set the global font family */
+  :global(body) {
+    font-family: 'Press Start 2P', sans-serif;
+  }
 
   main {
     display: flex;
@@ -17,10 +22,5 @@
     justify-content: center;
     height: 100vh;
     background-color: #282c34;
-    font-family: 'Press Start 2P', sans-serif; /* Apply the font here */
-  }
-
- * {
-    user-select: none;
   }
 </style>
